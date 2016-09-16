SynapseFI/SynapseFI-Ruby

commit 6fa0a776ed51c83c6f875e96aa4f503e85b52344

Refactored nodes, trans, and users

lib/synapse_pay_rest/api/nodes.rb
@@ -8,48 +8,31 @@ module SynapsePayRest
     end
 
     def create_node_path(node_id: nil)
-      path = '/users/%s' % @client.user_id + '/nodes'
-      if node_id
-        path += '/%s' %node_id
-      end
-      return path
+      path = ['/users', client.user_id, 'nodes' ]
+      path << node_id if node_id
+      return path.join('/')
     end
 
     def add(payload: raise("payload is required"))
       path = create_node_path()
-      response = @client.post(path, payload)
-      return response
+      client.post(path, payload)
     end
 
     def get(node_id: nil, page: nil)
-      if node_id
-        path = create_node_path(node_id: node_id)
-      else
-        path = create_node_path()
-      end
-      if page
-        path += '?page=%s' %page
-      end
-      response = @client.get(path)
-      return response
+      # If node_id is nil then create_node_path will handlie it just fine
+      path = create_node_path(node_id: node_id)
+      path += "?page=#{page}" if page
+      client.get(path)
     end
 
     def verify(node_id: nil, payload: raise("payload is required"))
-      if node_id
-        path = create_node_path(node_id: node_id)
-        response = @client.patch(path, payload)
-        return response
-      else
-        path = create_node_path()
-        response = @client.post(path, payload)
-        return response
-      end
+      path = create_node_path(node_id: node_id)
+      client.patch(path, payload)
     end
 
     def delete(node_id: raise("node_id is required"))
       path = create_node_path(node_id: node_id)
-      response = @client.delete(path)
-      return response
+      client.delete(path)
     end
   end
 end

lib/synapse_pay_rest/api/users.rb
@@ -4,6 +4,8 @@ require 'open-uri'
 
 module SynapsePayRest
   class Users
+    # Should refactor this to HTTPClient
+    VALID_QUERY_PARAMS = [:query, :page, :per_page].freeze
 
     attr_accessor :client
 
@@ -12,80 +14,64 @@ module SynapsePayRest
     end
 
     def create_user_path(user_id: nil)
-      path = '/users'
-      if user_id
-        path += '/%s' %user_id
-      end
-      return path
+      path = ['/users']
+      path << user_id if user_id
+      return path.join('/')
     end
 
     def refresh(payload: raise("payload is required"))
-      path = '/oauth/%s' % @client.user_id
+      path = "/oauth/#{@client.user_id}"
       response = @client.post(path, payload)
-      if response.has_key?('oauth_key')
-        @client.update_headers(oauth_key: response['oauth_key'])
-      end
+      client.update_headers(oauth_key: response['oauth_key']) if response['oauth_key']
       return response
     end
 
-    def get(user_id: nil, query: nil, page: nil, per_page: nil)
-      if user_id
-        path = create_user_path(user_id: user_id)
-        response = @client.get(path)
-        if response.has_key?('_id')
-          @client.update_headers(user_id: response['_id'])
-        end
-      elsif query
-        path += '?query=%s' %query
-        if page
-          path += '&page=%s' %page
-        end
-        if per_page
-          path += '&per_page=%s' %per_page
-        end
-      elsif page
-        path += '?page=%s' %page
-        if per_page
-          path += '&per_page=%s' %per_page
-        end
-      elsif per_page
-        path += '?page=%s' %per_page
-      else
-        path = create_user_path()
+    def get(options = {})
+      path = create_user_path(user_id: user_id)
+
+      if options[:user_id]
+        response = client.get(path)
+        client.update_headers(user_id: response['_id']) if response['_id']
+        return response
       end
-      response = @client.get(path)
-      return response
+
+      # Should factor this out into HTTPClient
+      params = VALID_QUERY_PARAMS.map do |p|
+        options[p] ? "#{p}=#{options[p]}" : nil
+      end.compact
+
+      # Probably should use CGI or RestClient's param builder instead of
+      # rolling our own, probably error-prone and untested version
+      # https://github.com/rest-client/rest-client#usage-raw-url
+      path += '?' + params.join('&') if params.any?
+      client.get(path)
     end
 
     def update(payload: raise("payload is required"))
-      path = create_user_path(user_id: @client.user_id)
-      response = @client.patch(path, payload)
-      return response
+      path = create_user_path(user_id: client.user_id)
+      client.patch(path, payload)
     end
 
     def create(payload: raise("payload is required"))
       path = create_user_path()
-      response = @client.post(path, payload)
-      return response
+      client.post(path, payload)
     end
 
     def add_doc(payload: raise("payload is required"))
-      path = create_user_path(user_id: @client.user_id)
-      response = @client.patch(path, payload)
-      return response
+      path = create_user_path(user_id: client.user_id)
+      client.patch(path, payload)
     end
 
     def answer_kba(payload: raise("payload is required"))
-      path = create_user_path(user_id: @client.user_id)
-      response = @client.patch(path, payload)
-      return response
+      path = create_user_path(user_id: client.user_id)
+      client.patch(path, payload)
     end
 
     def attach_file(file_path: raise("file_path is required"))
       path = create_user_path(user_id: @client.user_id)
-      file_contents = open(file_path) { |f| f.read }
+      file_contents = File.read(file_path)
       file_type = MIME::Types.type_for(file_path).first.content_type
-      mime_padding = 'data:' + file_type + ';base64,'
+      mime_padding = "data:#{file_type};base64,"
       encoded = Base64.encode64(file_contents)
       base64_attachment = mime_padding + encoded
       payload = {
@@ -93,8 +79,7 @@ module SynapsePayRest
           'attachment' => base64_attachment
         }
       }
-      response = @client.patch(path, payload)
-      return response
+      client.patch(path, payload)
     end
   end
 end

lib/synapse_pay_rest/api/trans.rb
@@ -8,44 +8,31 @@ module SynapsePayRest
     end
 
     def create_transaction_path(node_id: raise("node_id is required"), trans_id: nil)
-      path = '/users/' + @client.user_id + '/nodes/' + node_id + '/trans'
-      if trans_id
-        path += '/' + trans_id
-      end
-      return path
+      path = ['/users', client.user_id, 'nodes', node_id, 'trans' ]
+      path << trans_id if trans_id
+      return path.join('/')
     end
 
     def create(node_id: raise("node_id is required"), payload: raise("payload is required"))
       path = create_transaction_path(node_id: node_id)
-      response = @client.post(path, payload)
-      return response
+      client.post(path, payload)
     end
 
     def update(node_id: raise("node_id is required"), trans_id: raise("trans_id is required"), payload: raise("payload is required"))
       path = create_transaction_path(node_id: node_id, trans_id: trans_id)
-      response = @client.patch(path, payload)
-      return response
+      client.patch(path, payload)
     end
 
     def get(node_id: raise("node_id is required"), trans_id: nil, page: nil)
-      if trans_id
-        path = create_transaction_path(node_id: node_id, trans_id: trans_id)
-        response = @client.get(path)
-        return response
-      else
-        path = create_transaction_path(node_id: node_id)
-        if page
-          path += '?page=%d' %page
-        end
-        response = @client.get(path)
-        return response
-      end
+      path = create_transaction_path(node_id: node_id, trans_id: trans_id)
+      # This part really should just be a generic params generator
+      path += "?page=#{page}" if page
+      client.get(path)
     end
 
     def delete(node_id: raise("node_id is required"), trans_id: raise("trans_id is required"))
       path = create_transaction_path(node_id: node_id, trans_id: trans_id)
-      response = @client.delete(path)
-      return response
+      client.delete(path)
     end
   end
 end

commit ea9edf7290258fb4f31af879ab48930f2d092c66

Fix bug in Nodes#verify introduced by commit 6fa0a776ed

lib/synapse_pay_rest/api/nodes.rb
@@ -14,20 +14,28 @@ module SynapsePayRest
     end
 
     def add(payload: raise("payload is required"))
-      path = create_node_path()
+      path = create_node_path
       client.post(path, payload)
     end
 
     def get(node_id: nil, page: nil)
-      # If node_id is nil then create_node_path will handlie it just fine
+      # If node_id is nil then create_node_path will handle it just fine
       path = create_node_path(node_id: node_id)
       path += "?page=#{page}" if page
       client.get(path)
     end
 
     def verify(node_id: nil, payload: raise("payload is required"))
-      path = create_node_path(node_id: node_id)
-      client.patch(path, payload)
+      if node_id
+        # verify microdeposits
+        path = create_node_path(node_id: node_id)
+        response = @client.patch(path, payload)
+      else
+        # verify MFA question(s)
+        path = create_node_path()
+        response = @client.post(path, payload)
+      end
+      response
     end
 
     def delete(node_id: raise("node_id is required"))

lib/synapse_pay_rest/api/users.rb
@@ -53,7 +53,7 @@ module SynapsePayRest
     end
 
     def create(payload: raise("payload is required"))
-      path = create_user_path()
+      path = create_user_path
       response = client.post(path, payload)
       client.update_headers(user_id: response['_id']) if response['_id']
       return response

lib/synapse_pay_rest/http_client.rb
@@ -13,7 +13,7 @@ module SynapsePayRest
       @user_id = user_id
     end
 
-    def get_headers()
+    def get_headers
       user    = "#{config['oauth_key']}|#{config['fingerprint']}"
       gateway = "#{config['client_id']}|#{config['client_secret']}"
       headers = {
@@ -36,22 +36,22 @@ module SynapsePayRest
 
 
     def post(path, payload)
-      response = with_error_handling { RestClient.post(full_url(path), payload.to_json, get_headers()) }
+      response = with_error_handling { RestClient.post(full_url(path), payload.to_json, get_headers) }
       JSON.parse(response)
     end
 
     def patch(path, payload)
-      response = with_error_handling { RestClient.patch(full_url(path), payload.to_json, get_headers()) }
+      response = with_error_handling { RestClient.patch(full_url(path), payload.to_json, get_headers) }
       JSON.parse(response)
     end
 
     def get(path)
-      response = with_error_handling { RestClient.get(full_url(path), get_headers()) }
+      response = with_error_handling { RestClient.get(full_url(path), get_headers) }
       JSON.parse(response)
     end
 
     def delete(path)
-      response = with_error_handling { RestClient.delete(full_url(path), get_headers()) }
+      response = with_error_handling { RestClient.delete(full_url(path), get_headers) }
       JSON.parse(response)
     end
 
@@ -79,33 +79,33 @@ module SynapsePayRest
       when 500
         return e.response
       when 405
-        return handle_method_not_allowed()
+        return handle_method_not_allowed
       when 502
         #Raise a gateway error
-        return handle_gateway_error()
+        return handle_gateway_error
       when 504
         #Raise a timeout error
-        return handle_timeout_error()
+        return handle_timeout_error
       else
         #Raise a generic error
-        return handle_unknown_error()
+        return handle_unknown_error
       end
     end
 
-    def handle_method_not_allowed()
+    def handle_method_not_allowed
       return {'success' => false, 'reason' => 'The method is not allowed. Check your id parameters.'}.to_json
     end
 
-    def handle_gateway_error()
+    def handle_gateway_error
       return {'success' => false, 'reason' => 'The gateway appears to be down.  Check synapsepay.com or try again later.'}.to_json
     end
 
-    def handle_timeout_error()
+    def handle_timeout_error
       return {'success' => false, 'reason' => 'A timeout has occurred.'}.to_json
     end
 
 
-    def handle_unknown_error()
+    def handle_unknown_error
       return {'success' => false, 'reason' => 'Unknown error in library. Contact synapsepay.'}.to_json
     end
   end

test/error_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-
+require 'pry'; binding.pry
 class ErrorTest < Minitest::Test
 
   def test_400_error_code