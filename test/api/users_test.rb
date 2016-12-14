f31cc61b06daeb7e7eaed704455f461a6596fd58
stevula <brodericksteven@gmail.com>
2016-12-14 14:11:37 -0800

    Finish previous merge

--- test/api/users_test.rb
+++ test/api/users_test.rb
@@ -98,15 +98,8 @@ class UsersTest < Minitest::Test
   end
 
   def test_add_documents_via_kyc2_with_kba
-<<<<<<< HEAD
-    add_docs_response = @client.users.update(
-      user_id: @user['_id'],
-      payload: test_add_documents_kyc2_payload(virtual_docs: [test_kba_ssn_hash])
-    )
-=======
     skip 'deprecated'
     add_docs_response = @client.users.update(payload: test_add_documents_kyc2_payload(virtual_docs: [test_kba_ssn_hash]))
->>>>>>> qa
     base_document = add_docs_response['documents'].last
 
     assert_operator base_document.length, :>=, 4
