a38427cd6bf3327b1627beb7c7f35f7b2f618454
Tyna Huynh <tynahuynh@Tynas-MacBook-Pro-3.local>
2018-09-10 16:15:09 -0700

    statements

--- test/api/statements_test.rb
+++ test/api/statements_test.rb
@@ -2,12 +2,19 @@ require 'test_helper'
 
 class StatementsTest < Minitest::Test
   def setup
-    test_values   = test_client_with_statements
-    @client       = test_values[:client]
+     @options = {
+      client_id:        ENV.fetch('TEST_CLIENT_ID_STATEMENTS'),
+      client_secret:    ENV.fetch('TEST_CLIENT_SECRET_STATEMENTS'),
+      ip_address:       '127.0.0.1',
+      fingerprint:      'ed234f9a5dcaf00f9e231a7079ac9961',
+      development_mode: true
+    } 
+    @client = SynapsePayRest::Client.new(@options)
   end
 
   def test_statements_get
-    statements_response = @client.statements.get()
+  	user = SynapsePayRest::User.find(client: @client, id: '5b5f95dcf122e319740dbc76')
+    statements_response = @client.statements.get(user_id: user.id)
     assert_equal '0', statements_response['error_code']
     assert_equal '200', statements_response['http_code']
   end
--- test/model/statement_test.rb
+++ test/model/statement_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class StatementTest < Minitest::Test
   def setup
     @options = {
-      client_id:        ENV.fetch('TEST_CLIENT_ID'),
-      client_secret:    ENV.fetch('TEST_CLIENT_SECRET'),
+      client_id:        ENV.fetch('TEST_CLIENT_ID_STATEMENTS'),
+      client_secret:    ENV.fetch('TEST_CLIENT_SECRET_STATEMENTS'),
       ip_address:       '127.0.0.1',
-      fingerprint:      'test_fp',
+      fingerprint:      'ed234f9a5dcaf00f9e231a7079ac9961',
       development_mode: true
     } 
   end
@@ -15,10 +15,10 @@ class StatementTest < Minitest::Test
   def test_get_statement_by_user
     client = SynapsePayRest::Client.new(@options)
     user = SynapsePayRest::User.find(client: client, id: '5b5f95dcf122e319740dbc76')
-    response = SynapsePayRest::Statement.by_node(client: client, user: user)
+    response = SynapsePayRest::Statement.by_user(client: client, user: user)
 
-    refute_nil response.pdf_url
-    refute_nil response.csv_url
+    refute_nil response[1].pdf_url
+    refute_nil response[1].csv_url
   end
 
   def test_get_statement_by_node
@@ -27,8 +27,8 @@ class StatementTest < Minitest::Test
     node = user.find_node(id: '5b634a241374e5004c7b39b1')
     response = SynapsePayRest::Statement.by_node(client: client, node: node)
 
-    refute_nil response.csv_url
-    refute_nil response.pdf_url
+    refute_nil response[1].csv_url
+    refute_nil response[1].pdf_url
   end
 
 end
